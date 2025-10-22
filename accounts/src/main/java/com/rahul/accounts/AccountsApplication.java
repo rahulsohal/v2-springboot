5a7f4337b077e09c4424c3f1d3b33a9045286714
rahulsohal <rahulsohal.nith@gmail.com>
2025-10-22 23:42:39 +0530

    configuration with push based mechanism from git changes and dockerisation of services

--- accounts/src/main/java/com/rahul/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/rahul/accounts/AccountsApplication.java
@@ -1,6 +1,11 @@
 package com.rahul.accounts;
 
 import com.rahul.accounts.dto.AccountsContactInfo;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,7 +13,24 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
-@EnableConfigurationProperties(AccountsContactInfo.class)
+@EnableConfigurationProperties(value = {AccountsContactInfo.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Rahul Sohal",
+                        email = "rahul@mail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description =  "EazyBank Accounts microservice REST API Documentation"
+        )
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
